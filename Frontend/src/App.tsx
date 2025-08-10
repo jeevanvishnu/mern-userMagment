a0994adcc009e2a59c372511932cfd2fc3526957
jeevan vishnu <jeevanvishnu18@gmail.com>
2025-08-10 21:07:01 +0530

    Add Frontend part

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './pages/Home'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-center my-4 text-3xl '>User Managment</h1>
+     <Home/>
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/components/Table.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Table = () => {
+  return (
+    <div>
+        <div className="overflow-x-auto">
+  <table className="table">
+   
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>emil</th>
+        <th>Date</th>
+      </tr>
+    </thead>
+    <tbody>
+     
+      <tr className="bg-base-200">
+        <th>1</th>
+        <td>Cy Ganderton</td>
+        <td>Quality Control Specialist</td>
+        <td>Blue</td>
+      </tr>
+      
+      <tr>
+        <th>2</th>
+        <td>Hart Hagerty</td>
+        <td>Desktop Support Technician</td>
+        <td>Purple</td>
+      </tr>
+    
+      <tr>
+        <th>3</th>
+        <td>Brice Swyre</td>
+        <td>Tax Accountant</td>
+        <td>Red</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+    </div>
+  )
+}
+
+export default Table
