3e76ea213ce166324594cf4115d5f4830a89ad17
jeevan vishnu <jeevanvishnu18@gmail.com>
2025-08-11 23:02:35 +0530

    complete edit page design

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Home from './pages/Home'
 
+
 const App = () => {
   return (
     <div>
       <h1 className='text-center my-4 text-3xl '>User Managment</h1>
      <Home/>
-    </div>
+     </div>
   )
 }
 
---
+++ Frontend/src/components/EditForm.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const EditForm = () => {
+  return (
+    <div className="flex flex-col w-[400px] border h-[300px] items-center ">
+      <label className="my-5 input validator">
+        <svg
+          className="h-[1em] opacity-50"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <g
+            stroke-linejoin="round"
+            stroke-linecap="round"
+            stroke-width="2.5"
+            fill="none"
+            stroke="currentColor"
+          >
+            <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+            <circle cx="12" cy="7" r="4"></circle>
+          </g>
+        </svg>
+        <input type="text" required placeholder="Username" />
+      </label>
+
+      <label className="input validator mb-6">
+        <svg
+          className="h-[1em] opacity-50"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <g
+            stroke-linejoin="round"
+            stroke-linecap="round"
+            stroke-width="2.5"
+            fill="none"
+            stroke="currentColor"
+          >
+            <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+            <circle cx="12" cy="7" r="4"></circle>
+          </g>
+        </svg>
+        <input type="text" required placeholder="Email" />
+      </label>
+      <button className=" btn btn-soft btn-success w-[200px]">Success</button>
+    </div>
+  );
+};
+
+export default EditForm;
